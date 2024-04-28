50d543ce9e3a9f09612905e0bee71a627552c12a
Mrtrengh <165410866+Mrtrengh@users.noreply.github.com>
2024-04-28 13:21:25 +0300

    Add files via upload

---
+++ registersystem.js
@@ -0,0 +1,66 @@
+let password = Number(prompt("Your password must be at least 4 characters."))
+
+password;
+
+let passwordlenght = password.lenght
+
+passwordcontrol;
+
+function passwordcontrol(){
+
+    if(passwordlenght == 4){
+        console.log("The record has been created successfully.")
+    }else {
+        let sifre = Number(prompt("Your password must be at least 4 characters."))
+    
+        
+    }
+    
+    return password;
+}
+
+switch(password);
+
+case 1234;
+
+alert("Your password was not accepted because it was too easy.");
+let password = Number(prompt("Please enter your new password."));
+break;
+
+case 4321; 
+
+alert("Your password was not accepted because it was too easy.");
+let password = Number(prompt("Please enter your new password."));
+break;
+
+case sifre;
+
+alert("Your password was not accepted because it was too easy.");
+let password = Number(prompt("Please enter your new password."));
+break;
+
+case 1111;
+
+alert("Your password was not accepted because it was too easy.");
+let password = Number(prompt("Please enter your new password."));
+break;
+
+default;
+
+confirm("Your password has been saved.");
+break;
+
+let namearray = ["Name1","name2"];
+
+let username = prompt("please enter your username.")
+
+let reply = namearray.includes(username);
+
+if(reply == false){
+    namearray.push(ad)
+  console.log("Name entry successful.")
+}else {
+    alert("The username is being used by someone else.");
+}
+
+
